48c3e7f20523e7d59e72ebc6f7442a8bb7c8c8f2
Thomas Raducha <thomasraducha@gmail.com>
2023-01-19 22:03:36 +0100

    add_button_modal

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ function displayMovieList(movies) {
             <p>${movies[idx].Year}</p>
         </div>
         `;
+
         searchList.appendChild(movieListItem);
     }
     loadMovieDetails();
@@ -73,8 +74,59 @@ function displayMovieDetails(details) {
         <p class = "plot"> ${details.Plot}</p>
         <p class = "language"><b>Language:</b> ${details.Language}</p>
         <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+        <!-- Trigger/Open The Modal -->
+        <button id="myBtn">Open Modal</button>
+        
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
+        
+          <!-- Modal content -->
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <div class = "movie-poster">
+            <img src = "${(details.Poster != "N/A") ? details.Poster : "image_not_found.png"}" alt = "movie poster">
+        </div>
+        <div class = "movie-info">
+            <h3 class = "movie-title">${details.Title}</h3>
+            <ul class = "movie-misc-info">
+                <li class = "released">Date de sortie: ${details.Released}</li>
+            </ul>
+            <p class = "genre"><b>Genre:</b> ${details.Genre}</p>
+            <p class = "writer"><b>Écrivain:</b> ${details.Writer}</p>
+            <p class = "actors"><b>Acteurs: </b>${details.Actors}</p>
+            <p class = "plot"> ${details.Plot}</p>
+            <p class = "language"><b>Language:</b> ${details.Language}</p>
+            <p class = "awards"><b><i class = "fas fa-award"></i></b> ${details.Awards}</p>
+                  </div>
+        
+        </div>        
     </div>
     `;
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
 }
 
 
